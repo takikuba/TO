45573905e71af57bdb36a28206d3d94735bdd965
takikuba <jakub26@gmail.com>
2020-10-15 12:02:27 +0200

    Add the exchange rate

--- LAb1/src/Screen.java
+++ LAb1/src/Screen.java
@@ -9,14 +9,15 @@ public class Screen  {
     static double getCash(String val1, String val2, Double val3) throws Exception {
         double[] info1 = getLine(val1);
         double[] info2 = getLine(val2);
-//            System.out.println(info1[0] + " " + info1[1]);
-//            System.out.println(info2[0] + " " + info2[1]);
 
-        double reval = (info1[0] / info1[1]) / (info2[0] / info2[1]) * val3;
-//            System.out.println(reval);
-        return reval;
-
+        return (info1[0] / info1[1]) / (info2[0] / info2[1]) * val3;
     }
+
+    static double getRate(String val1) throws Exception{
+        double[] info1 = getLine(val1);
+        return info1[0];
+    }
+
     static double[] getLine(String var) throws Exception{
         new URLReader();
         BufferedReader data = URLReader.getData();
@@ -37,10 +38,10 @@ public class Screen  {
 
         }
         data.close();
-        double []reval = {0, 0};
-        reval[0] = kurs;
-        reval[1] = przelicznik;
-        return reval;
+        double []retval = {0, 0};
+        retval[0] = kurs;
+        retval[1] = przelicznik;
+        return retval;
     }
 
     public static void main(String []args) {
@@ -49,6 +50,8 @@ public class Screen  {
 
         Label label1 = new Label("Wybierz walute do wymiany: ");
         label1.setBounds(15, 20, 150, 30);
+        Label label12 = new Label();
+        label12.setBounds(250, 45, 150, 20);
 
         Choice choice1;
         choice1 = new CashList().getCashList();
@@ -60,14 +63,22 @@ public class Screen  {
         button1.addActionListener((e)->{
             val1[0] = choice1.getItem(choice1.getSelectedIndex());
             label1.setText("Waluta do wymiany: " + val1[0]);
+            try {
+                label12.setText("Aktualny kurs: " + getRate(val1[0]));
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
         });
 
         jFrame.getContentPane().add(button1);
         jFrame.getContentPane().add(choice1);
+        jFrame.getContentPane().add(label12);
         jFrame.getContentPane().add(label1);
 
         Label label2 = new Label("Wybierz walute na ktora chcesz wymienic: ");
         label2.setBounds(15, 75, 250, 30);
+        Label label22 = new Label();
+        label22.setBounds(250, 100, 150, 20);
 
         Choice choice2 = new CashList().getCashList();
         choice2.setBounds(40, 100, 75, 75);
@@ -78,11 +89,17 @@ public class Screen  {
         button2.addActionListener((e)->{
             val2[0] = choice2.getItem(choice2.getSelectedIndex());
             label2.setText("Wymieniam na: " + val2[0]);
+            try {
+                label22.setText("Aktualny kurs: " + getRate(val2[0]));
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            }
         });
 
         jFrame.getContentPane().add(button2);
         jFrame.getContentPane().add(choice2);
         jFrame.getContentPane().add(label2);
+        jFrame.getContentPane().add(label22);
 
         Label label3 = new Label("Podaj kwote do wymiany: [.]");
         label3.setBounds(20, 160, 200, 20);
@@ -99,7 +116,7 @@ public class Screen  {
         button3.addActionListener((e)->{
             val3[0] = Double.valueOf(textField.getText());
             try {
-                label4.setText(String.valueOf("Otrzymasz: " + String.format("%.2f", getCash(val1[0], val2[0], val3[0]))) + val2[0]);
+                label4.setText("Otrzymasz: " + String.format("%.2f", getCash(val1[0], val2[0], val3[0])) + " " + val2[0]);
             } catch (Exception exception) {
                 exception.printStackTrace();
             }
@@ -109,7 +126,6 @@ public class Screen  {
         jFrame.getContentPane().add(label4);
         jFrame.getContentPane().add(textField);
         jFrame.getContentPane().add(button3);
-
 
         jFrame.setLocationRelativeTo(null);
         jFrame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
@@ -127,45 +143,14 @@ class URLReader {
 }
 
 class CashList extends Choice {
-
     Choice getCashList(){
-        Choice choice1 = new Choice();
-        choice1.add("THB");
-        choice1.add("USD");
-        choice1.add("AUD");
-        choice1.add("HKD");
-        choice1.add("CAD");
-        choice1.add("NZD");
-        choice1.add("SGD");
-        choice1.add("EUR");
-        choice1.add("HUF");
-        choice1.add("CHF");
-        choice1.add("GBP");
-        choice1.add("UAH");
-        choice1.add("JPY");
-        choice1.add("CZK");
-        choice1.add("DKK");
-        choice1.add("ISK");
-        choice1.add("NOK");
-        choice1.add("SEK");
-        choice1.add("HRK");
-        choice1.add("RON");
-        choice1.add("BGN");
-        choice1.add("TRY");
-        choice1.add("ILS");
-        choice1.add("CLP");
-        choice1.add("PHP");
-        choice1.add("MXN");
-        choice1.add("ZAR");
-        choice1.add("BRL");
-        choice1.add("MYR");
-        choice1.add("RUB");
-        choice1.add("IDR");
-        choice1.add("INR");
-        choice1.add("KRW");
-        choice1.add("CNY");
-        choice1.add("XRD");
-        return choice1;
+        Choice choice = new Choice();
+        String[] cash = {"THB", "USD", "AUD", "HKD", "CAD", "NZD", "SGD", "EUR", "HUF", "CHF", "GBP", "UAH",
+                         "JPY", "CZK", "DKK", "ISK", "NOK", "SEK", "HRK", "RON", "BGN", "TRY", "ILS", "CLP",
+                         "PHP", "MXN", "ZAR", "BRL", "MYR", "RUB", "IDR", "INR", "KRW", "CNY", "XRD"};
+        for(String s: cash){
+            choice.add(s);
+        }
+        return choice;
     }
-
 }
